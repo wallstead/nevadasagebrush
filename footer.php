dcd44226a729c4c286f73a43f5295bc944753a2a
Willis Allstead <willallstead@icloud.com>
2017-08-14 17:46:43 -0700

    added copyright info

--- footer.php
+++ footer.php
@@ -4,6 +4,9 @@
 							<?php vw_render_site_social_icons(); ?>
 						</div>
 						<img class="logo" src="/wp-content/uploads/2017/08/2Artboard-1@2x-1.png"> <!-- white logo -->
+						<div class="copyrightstuff">
+							<p>&copy; Copyright 2017, The Nevada Sagebrush</p>
+						</div>
 					</div>
 				</footer>
 
