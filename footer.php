f3c451f1b18742f69f125cbb7efe2f450a345f71
Willis Allstead <willallstead@icloud.com>
2017-08-14 18:00:10 -0700

    added transition in footer

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 						<div class="social">
 							<?php vw_render_site_social_icons(); ?>
 						</div>
-						<img class="logo" src="/wp-content/uploads/2017/08/2Artboard-1@2x-1.png"> <!-- white logo -->
+						<img class="logo" src="/wp-content/uploads/2017/08/2Artboard-1@2x-1.png" alt="The Nevada Sagebrush white logo"> <!-- white logo -->
 						<div class="copyrightstuff">
 							<p>&copy; Copyright 2017, The Nevada Sagebrush</p>
 							<?php
