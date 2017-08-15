a953bc7dcfc62010e7ab4f131fe503622ba5f735
Willis Allstead <willallstead@icloud.com>
2017-08-14 17:38:26 -0700

    added white logo to footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 				<footer id="footer">
 					<div class="footer-content">
-						<img src="/wp-content/uploads/2017/08/2Artboard-1@2x-1.png">
+						<img class="logo" src="/wp-content/uploads/2017/08/2Artboard-1@2x-1.png"> <!-- white logo -->
 					</div>
 				</footer>
 
