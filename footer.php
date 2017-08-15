3ee81e3dcae790d7638f56092c86dd0137a620aa
Willis Allstead <willallstead@icloud.com>
2017-08-14 17:51:04 -0700

    testing top nav in footer

--- footer.php
+++ footer.php
@@ -6,6 +6,20 @@
 						<img class="logo" src="/wp-content/uploads/2017/08/2Artboard-1@2x-1.png"> <!-- white logo -->
 						<div class="copyrightstuff">
 							<p>&copy; Copyright 2017, The Nevada Sagebrush</p>
+							<?php
+							if ( has_nav_menu('top_navigation' ) ) {
+								wp_nav_menu( array(
+									'theme_location' => 'top_navigation',
+									'container' => false,
+									'menu_class' => 'top-nav list-unstyled',
+									'link_before' => '<span>',
+									'link_after' => '</span>',
+									'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+									'depth' => 2,
+									'walker' => new vw_text_menu_walker()
+								) );
+							}
+							?>
 						</div>
 					</div>
 				</footer>
