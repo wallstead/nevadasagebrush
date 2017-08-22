1962ca01c6762ee4030d94f56ad167448e0bb2bc
Willis Allstead <willallstead@icloud.com>
2017-08-22 15:23:10 -0700

    added placeholder images to slider

--- index.php
+++ index.php
@@ -57,15 +57,7 @@
 
 <script>
 	$(document).ready(function() {
-		$(".featuredSlider").owlCarousel({
-		  loop: true,
-		  autoplay: true,
-		  items: 1,
-		  nav: true,
-		  autoplayHoverPause: true,
-		  animateOut: 'slideOutUp',
-		  animateIn: 'slideInUp'
-		});
+		$(".featuredSlider").owlCarousel();
 	});
 </script>
 
