f0fc1e67f770a12b97043d809705d9c3b9bda197
Willis Allstead <willallstead@icloud.com>
2017-08-22 12:17:09 -0700

    styling for categories on home page

--- index.php
+++ index.php
@@ -2,17 +2,19 @@
 
 <div id="page-wrapper" class="container">
 	<div id="page-content">
-		<div class="categoryTitle">
-			<h3>News</h3>
-		</div>
-		<div class="categoryTitle">
-			<h3>A &amp; E</h3>
-		</div>
-		<div class="categoryTitle">
-			<h3>Opinion</h3>
-		</div>
-		<div class="categoryTitle">
-			<h3>Sports</h3>
+		<div class="categories">
+			<div class="categoryTitle">
+				<h3>News</h3>
+			</div>
+			<div class="categoryTitle">
+				<h3>A &amp; E</h3>
+			</div>
+			<div class="categoryTitle">
+				<h3>Opinion</h3>
+			</div>
+			<div class="categoryTitle">
+				<h3>Sports</h3>
+			</div>
 		</div>
 	</div>
 </div>
