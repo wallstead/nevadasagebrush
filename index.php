704b8e9f703a7e6b4f575409e3e489a9528b5e70
Willis Allstead <willallstead@icloud.com>
2017-08-22 14:57:28 -0700

    added link to category pages

--- index.php
+++ index.php
@@ -27,19 +27,19 @@
 
 			?>
 			<div class="category">
-				<div class="categoryTitle"><h3>News</h3></div>
+				<a href="/blog/category/news/"><div class="categoryTitle"><h3>News</h3></div></a>
 				<?php listStoriesOfCategory('News'); ?>
 			</div>
 			<div class="category">
-				<div class="categoryTitle"><h3>A &amp; E</h3></div>
+				<a href="/blog/category/arts-entertainment/"><div class="categoryTitle"><h3>A &amp; E</h3></div></a>
 				<?php listStoriesOfCategory('Arts & Entertainment'); ?>
 			</div>
 			<div class="category">
-				<div class="categoryTitle"><h3>Opinion</h3></div>
+				<a href="/blog/category/opinion/"><div class="categoryTitle"><h3>Opinion</h3></div></a>
 				<?php listStoriesOfCategory('Opinion'); ?>
 			</div>
 			<div class="category">
-				<div class="categoryTitle"><h3>Sports</h3></div>
+				<a href="/blog/category/sports/"><div class="categoryTitle"><h3>Sports</h3></div></a>
 				<?php listStoriesOfCategory('Sports'); ?>
 			</div>
 		</div>
