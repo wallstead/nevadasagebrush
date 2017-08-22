7ffa6174c98c99be5c9c268fc674650f6c304eb5
Willis Allstead <willallstead@icloud.com>
2017-08-22 12:14:15 -0700

    added category titles for runoff stories

--- index.php
+++ index.php
@@ -2,7 +2,18 @@
 
 <div id="page-wrapper" class="container">
 	<div id="page-content">
-		<h4>tester</h4>
+		<div class="categoryTitle">
+			<h3>News</h3>
+		</div>
+		<div class="categoryTitle">
+			<h3>A &amp; E</h3>
+		</div>
+		<div class="categoryTitle">
+			<h3>Opinion</h3>
+		</div>
+		<div class="categoryTitle">
+			<h3>Sports</h3>
+		</div>
 	</div>
 </div>
 
