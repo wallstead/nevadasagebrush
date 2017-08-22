ec8bf8ab74f81b99c4dd7e8c469ce4747ddfeb8a
Willis Allstead <willallstead@icloud.com>
2017-08-22 13:51:26 -0700

    added better way of getting post author

--- index.php
+++ index.php
@@ -11,13 +11,17 @@
 				<?php global $post; // required
 					$args = array('numberposts' => 3, 'category_name' => 'news');
 					$custom_posts = get_posts($args);
+					$counter = 0;
 					foreach($custom_posts as $post) : setup_postdata($post);
 							$author_id = $post->post_author;
 							echo '<div class="storyOfCategory">';
-
+							if ($counter < 1) {
+								echo '<img src="'.get_the_post_thumbnail_url($post->ID, 'post-thumbnail' ).'" alt="'.get_the_title($post->ID).'">';
+							}
 							echo '<div class="storyInfo"><p>By '.get_the_author_meta( 'display_name', $author_id ).'</p><p>'.get_the_date('m/d/y', $post->ID).'</p></div>';
 							echo '<h4>'.get_the_title($post->ID).'</h4>';
 							echo '</div>';
+							$counter++;
 					endforeach;
 
 					wp_reset_postdata();
