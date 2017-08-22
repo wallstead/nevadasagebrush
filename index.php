92e411892cc3ce8289963dba63c228eb1d5dc9b6
Willis Allstead <willallstead@icloud.com>
2017-08-22 14:08:24 -0700

    added gutter to categories

--- index.php
+++ index.php
@@ -27,19 +27,19 @@
 
 			?>
 			<div class="category">
-				<h3>News</h3>
+				<div class="categoryTitle"><h3>News</h3></div>
 				<?php listStoriesOfCategory('News'); ?>
 			</div>
 			<div class="category">
-				<h3>A &amp; E</h3>
+				<div class="categoryTitle"><h3>A &amp; E</h3></div>
 				<?php listStoriesOfCategory('Arts & Entertainment'); ?>
 			</div>
 			<div class="category">
-				<h3>Opinion</h3>
+				<div class="categoryTitle"><h3>Opinion</h3></div>
 				<?php listStoriesOfCategory('Opinion'); ?>
 			</div>
 			<div class="category">
-				<h3>Sports</h3>
+				<div class="categoryTitle"><h3>Sports</h3></div>
 				<?php listStoriesOfCategory('Sports'); ?>
 			</div>
 		</div>
