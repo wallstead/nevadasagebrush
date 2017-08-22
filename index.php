3fbecfcd19520e19c92dabc85eec745d2047b32d
Willis Allstead <willallstead@icloud.com>
2017-08-22 12:11:35 -0700

    removed rwo

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php get_header(); ?>
 
 <div id="page-wrapper" class="container">
-	<div class="row">
-		<div id="page-content">
-			<h4>tester </h4>
-		</div>
+	<div id="page-content">
+		<h4>tester</h4>
 	</div>
 </div>
 
