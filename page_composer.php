70ca168158ca9b8e1d88d6e0beb3255fa5ec5f6b
Willis Allstead <willallstead@icloud.com>
2017-08-22 12:03:49 -0700

    testt

--- page_composer.php
+++ page_composer.php
@@ -5,8 +5,9 @@ Template Name: Page Composer
 get_header(); ?>
 
 <?php if ( have_posts() ) : the_post(); ?>
-	
+
 <div id="page-wrapper" class="container">
+	<h1>testttt</h1>
 	<?php vwpc_render(); ?>
 </div>
 <?php endif; ?>
