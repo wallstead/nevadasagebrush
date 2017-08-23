70b143a98b46e4945ac6bf8494dfa6426c72dc4c
Willis Allstead <willallstead@icloud.com>
2017-08-23 11:43:34 -0700

    adding newsletter box

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
                     $array = json_decode( $body, true );
                     if( ! empty( $array ) ) {
                         foreach($array['response']['docs'] as $doc) {
-                            echo '<a href="https://issuu.com/nevadasagebrush/docs/'.$doc['docname'].'"><div class="recent-journal index-'.$counter.'"><p>'.$doc['title'].'</p><img src="https://image.isu.pub/'.$doc['documentId'].'/jpg/page_1_thumb_large.jpg" alt="'.$doc['title on Issuu'].'"></div></a>';
+                            echo '<a href="https://issuu.com/nevadasagebrush/docs/'.$doc['docname'].'"><div class="recent-journal index-'.$counter.'"><img src="https://image.isu.pub/'.$doc['documentId'].'/jpg/page_1_thumb_large.jpg" alt="'.$doc['title on Issuu'].'"></div></a>';
                         }
                     }
                 }
